96e4450e2d8751afc7a498908eefb6e2a62e9e70
tse <markus@herrthese.de>
2016-05-12 16:02:28 +0200

    [check]  added the logic including redirect

--- lib/website-health-checker.rb
+++ lib/website-health-checker.rb
@@ -2,21 +2,33 @@
 
 require 'json'
 require 'net/http'
+require "net/https"
+require 'pp'
 
 class WebsiteHealthChecker
-  HOST = 'foo'
 
-  def self.check_hosts
-    # resp = Net::HTTP.get_response(URI.parse(UPDATE_CENTER))
-    # data = resp.body.gsub(/updateCenter\.post\(\n/,'').gsub(/\n\);/,'')
-    # JSON.parse(data)
+  def self.check_host(host)
+    msg = host
+    sep = " -> "
+    resp = Net::HTTP.get_response(URI.parse("http://#{host}"))
+    msg = msg << sep << resp.code
+    if resp.code == "301"
+      msg = msg << sep << resp.header['location']
+      resp = Net::HTTP.get_response(URI.parse(resp.header['location']))
+      msg = msg << sep << resp.code
+    end
+    pp msg
   end
 
-  def self.check
-    check_hosts
+  def self.check(hosts)
+    hosts.each do | host |
+      check_host(host)
+    end
   end
 
   def self.healthcheck
     puts 'eat healthy'
   end
 end
+
+#WebsiteHealthChecker.check(['tse-c.de', 'herrthese.de'])
